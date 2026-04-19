b9e29f72324df27198c66a20b59a37d5e96b4ba1
Sahrul Ramadhani <244107020058@student.polinema.ac.id>
2026-04-19 19:42:01 +0700

    fix: update HomeController - add event(), remove kejepret() & koleksi()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,19 +11,14 @@ class HomeController extends Controller
         return view('home');
     }
 
-    public function kejepret()
+    public function event()
     {
-        return view('kejepret');
+        return view('event');
     }
 
     public function search()
     {
         return view('search');
-    }
-
-    public function koleksi()
-    {
-        return view('koleksi');
     }
 
     public function profil()
